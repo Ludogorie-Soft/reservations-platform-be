4c3257f9edf6fde2ad29638f335f1befc8f5e7e7
ignatIgnatov <ignatov_rs@abv.bg>
2024-08-28 18:42:24 +0300

    initialize the project

---
+++ src/main/java/ludogorie_soft/reservations_platform_api/ReservationsPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package ludogorie_soft.reservations_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsPlatformApiApplication.class, args);
+	}
+
+}
