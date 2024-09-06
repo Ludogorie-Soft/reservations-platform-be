c6d61277597438fca5ef523e5fd295637c0ffdf1
Sherri Ase <shirin.asenova90@gmail.com>
2024-09-06 09:42:19 +0300

    fixed scurity settings

--- src/main/java/ludogorie_soft/reservations_platform_api/config/SecurityConfig.java
+++ src/main/java/ludogorie_soft/reservations_platform_api/config/SecurityConfig.java
@@ -1,39 +1,40 @@
 package ludogorie_soft.reservations_platform_api.config;
 
 import lombok.AllArgsConstructor;
-import ludogorie_soft.reservations_platform_api.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@AllArgsConstructor
 @Configuration
+@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
 
-    private UserService userService;
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers("/admin/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/register").permitAll();
                     authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers("/products/drinks").permitAll();
                     authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                 })
                 .httpBasic(Customizer.withDefaults());
@@ -46,4 +47,21 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails ramesh = User.builder()
+//                .username("ramesh")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(ramesh, admin);
+//    }
 }
