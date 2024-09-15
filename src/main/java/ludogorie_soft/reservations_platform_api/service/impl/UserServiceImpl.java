8d9e2754fc6a4a3af536b8930ed0d8a58bf1b248
Sherri Ase <shirin.asenova90@gmail.com>
2024-09-15 15:31:52 +0300

    removed role check

--- src/main/java/ludogorie_soft/reservations_platform_api/service/impl/UserServiceImpl.java
+++ src/main/java/ludogorie_soft/reservations_platform_api/service/impl/UserServiceImpl.java
@@ -39,16 +39,19 @@ public class UserServiceImpl implements UserService {
         }
         User user = userMapper.toEntity(registerDto);
         Set<Role> roles = new HashSet<>();
-        Optional<Role> optionalUserRole = Optional.ofNullable(roleRepository.findByName("ROLE_USER"));
-        Role userRole;
-        if (optionalUserRole.isPresent()) {
-            userRole = optionalUserRole.get();
-        } else {
-            userRole = new Role();
-            userRole.setName("ROLE_USER");
-            roleRepository.save(userRole);
-        }
-        roles.add(userRole);
+        Role role = new Role();
+        role.setName("ROLE_USER");
+        roles.add(role);
+//        Optional<Role> optionalUserRole = Optional.ofNullable(roleRepository.findByName("ROLE_USER"));
+//        Role userRole;
+//        if (optionalUserRole.isPresent()) {
+//            userRole = optionalUserRole.get();
+//        } else {
+//            userRole = new Role();
+//            userRole.setName("ROLE_USER");
+//            roleRepository.save(userRole);
+//        }
+//        roles.add(userRole);
         user.setRoles(roles);
         userRepository.save(user);
         return "User Registered Successfully!";
