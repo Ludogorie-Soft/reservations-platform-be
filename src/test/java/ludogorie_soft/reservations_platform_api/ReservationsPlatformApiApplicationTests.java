9241def57a01cb83620fefa9a51827c845f48dab
Sherri Ase <shirin.asenova90@gmail.com>
2024-09-15 15:46:21 +0300

    removed contextLoands()

--- src/test/java/ludogorie_soft/reservations_platform_api/ReservationsPlatformApiApplicationTests.java
+++ src/test/java/ludogorie_soft/reservations_platform_api/ReservationsPlatformApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReservationsPlatformApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
