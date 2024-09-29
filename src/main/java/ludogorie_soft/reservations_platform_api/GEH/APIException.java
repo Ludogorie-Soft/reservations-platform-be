55a3e21b9ae381230142a6eaa99bc6a576ba4cf2
Metin Ahmedov <129603823+metotg@users.noreply.github.com>
2024-09-29 13:38:29 +0300

    global exception handler

---
+++ src/main/java/ludogorie_soft/reservations_platform_api/GEH/APIException.java
@@ -0,0 +1,17 @@
+package ludogorie_soft.reservations_platform_api.GEH;
+
+
+import org.springframework.http.HttpStatus;
+
+public class APIException extends RuntimeException {
+    private final HttpStatus status;
+
+    public APIException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/ludogorie_soft/reservations_platform_api/GEH/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package ludogorie_soft.reservations_platform_api.GEH;
+
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
