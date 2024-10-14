a648cb6a916feac11de3c225885ef7ea9d0df0b1
HristoSL <hristoivanovslavchev@gmail.com>
2024-10-14 22:02:28 +0300

    fixes to package imports

--- src/test/java/ludogorie_soft/reservations_platform_api/controller/PropertyControllerTest.java
+++ src/test/java/ludogorie_soft/reservations_platform_api/controller/PropertyControllerTest.java
@@ -16,8 +16,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 class PropertyControllerTest {
 
--- src/test/java/ludogorie_soft/reservations_platform_api/service/impl/PropertyServiceImplTest.java
+++ src/test/java/ludogorie_soft/reservations_platform_api/service/impl/PropertyServiceImplTest.java
@@ -24,9 +24,20 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 public class PropertyServiceImplTest {
