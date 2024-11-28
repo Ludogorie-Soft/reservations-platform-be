60b433ca61d8d98ddf879ed66c16898794f9b316
Sherri Ase <shirin.asenova90@gmail.com>
2024-11-28 13:31:30 +0200

    add passing tests

--- src/test/java/ludogorie_soft/reservations_platform_api/controller/PaymentControllerIntegrationTest.java
+++ src/test/java/ludogorie_soft/reservations_platform_api/controller/PaymentControllerIntegrationTest.java
@@ -2,17 +2,17 @@ package ludogorie_soft.reservations_platform_api.controller;
 
 import ludogorie_soft.reservations_platform_api.dto.BookingRequestDto;
 import ludogorie_soft.reservations_platform_api.dto.BookingResponseDto;
-import ludogorie_soft.reservations_platform_api.entity.Booking;
-import ludogorie_soft.reservations_platform_api.entity.Property;
-import ludogorie_soft.reservations_platform_api.exception.BookingNotFoundException;
+import ludogorie_soft.reservations_platform_api.dto.PropertyRequestDto;
+import ludogorie_soft.reservations_platform_api.dto.PropertyResponseDto;
+import ludogorie_soft.reservations_platform_api.dto.RegisterDto;
 import ludogorie_soft.reservations_platform_api.helper.BookingTestHelper;
 import ludogorie_soft.reservations_platform_api.helper.PropertyTestHelper;
+import ludogorie_soft.reservations_platform_api.helper.UserTestHelper;
 import ludogorie_soft.reservations_platform_api.repository.BookingRepository;
 import ludogorie_soft.reservations_platform_api.repository.PropertyRepository;
-import ludogorie_soft.reservations_platform_api.service.BookingService;
+import ludogorie_soft.reservations_platform_api.repository.UserRepository;
 import ludogorie_soft.reservations_platform_api.service.PaymentService;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,126 +20,119 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class PaymentControllerIntegrationTest {
     private static final String BASE_BOOKING_URL = "/api/bookings";
+    private static final String BASE_PROPERTY_URL = "/api/properties";
+    private static final String REGISTER_URL = "/auth/register";
+    private static final String BASE_PAYMENT_URL = "/payment/create-payment-intent/";
 
     @Autowired
     private TestRestTemplate testRestTemplate;
-    @MockBean
-    private BookingService bookingService;
-
-    @MockBean
-    private PaymentService paymentService;
 
     @Autowired
-    private PaymentController paymentController;
+    private UserRepository userRepository;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private PropertyRepository propertyRepository;
 
     @Autowired
     private BookingRepository bookingRepository;
 
+    @MockBean
+    private PaymentService paymentService;
+
+    private RegisterDto registerDto;
     private BookingRequestDto bookingRequestDto;
-    private Booking booking;
+    private PropertyRequestDto propertyRequestDto;
+    private HttpHeaders headers;
+
     @BeforeEach
     void setup() {
+        registerDto = UserTestHelper.createRegisterDto();
         bookingRequestDto = BookingTestHelper.createBookingRequest();
+        propertyRequestDto = PropertyTestHelper.createDefaultPropertyRequestDto();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        createUserInDb();
+        ResponseEntity<PropertyResponseDto> propertyResponse = createPropertyInDb();
+        bookingRequestDto.setPropertyId(Objects.requireNonNull(propertyResponse.getBody()).getId());
     }
 
     @AfterEach
     void cleanUp() {
-        // Clean up the repository after each test
         bookingRepository.deleteAll();
+        propertyRepository.deleteAll();
+        userRepository.deleteAll();
+        headers = new HttpHeaders();
     }
+
+    @Test
+    void createPaymentIntent_ShouldReturnValidResponse_WhenBookingExists() {
+        //GIVEN
+        ResponseEntity<BookingResponseDto> responseBooking = createBookingInDb();
+        UUID bookingId = Objects.requireNonNull(responseBooking.getBody()).getId();
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        //WHEN
+        ResponseEntity<Map> response = testRestTemplate.postForEntity(
+                BASE_PAYMENT_URL + bookingId, request, Map.class);
+
+        //THEN
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        System.out.println("body " + response.getBody());
+    }
+
+    @Test
+    void testCreatePaymentShouldThrowWhenStripeThirdPartyNotWork() {
+        //GIVEN
+        ResponseEntity<BookingResponseDto> responseBooking = createBookingInDb();
+        UUID bookingId = Objects.requireNonNull(responseBooking.getBody()).getId();
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        Mockito.when(paymentService.createPaymentIntent(Mockito.any()))
+                .thenThrow(new RuntimeException("Simulated Stripe API failure"));
+
+        //WHEN
+        ResponseEntity<Map> response = testRestTemplate.postForEntity(
+                BASE_PAYMENT_URL + bookingId, request, Map.class);
+
+        //THEN
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
     private ResponseEntity<BookingResponseDto> createBookingInDb() {
         return testRestTemplate
                 .postForEntity(BASE_BOOKING_URL, bookingRequestDto, BookingResponseDto.class);
     }
-    @Test
-    void createPaymentIntent_ShouldReturnValidResponse_WhenBookingExists() {
-        ResponseEntity<BookingResponseDto> responseBooking = createBookingInDb();
-        BookingResponseDto bookingResponseDto = responseBooking.getBody();
 
-        UUID bookingId = bookingResponseDto.getId();
-        String url = "/payment/create-payment-intent/" + bookingId;
-        System.out.println("id " + bookingId);
-        System.out.println("url " + url);
-        System.out.println(booking.getTotalPrice());
-        Map<String, Object> mockResponse = new HashMap<>();
-        mockResponse.put("paymentIntentId", "pi_test123");
-        mockResponse.put("clientSecret", "secret_test123");
-//        when(paymentService.createPaymentIntent(Mockito.any(String.class)))
-//                .thenReturn(mockResponse);
-        ResponseEntity<Map> response = restTemplate.postForEntity(url, null, Map.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
-        assertEquals("pi_test123", response.getBody().get("paymentIntentId"));
-        assertEquals("secret_test123", response.getBody().get("clientSecret"));
+    private ResponseEntity<String> createUserInDb() {
+        return testRestTemplate
+                .postForEntity(REGISTER_URL, registerDto, String.class);
     }
 
-
-    @Test
-    void createPaymentIntent_ShouldHandleInvalidBookingId() {
-        // GIVEN
-
-        UUID invalidBookingId = UUID.randomUUID();
-
-        when(bookingService.getBooking(eq(invalidBookingId)))
-                .thenThrow(new BookingNotFoundException("Booking with id " + invalidBookingId + " not found!"));
-
-        // WHEN & THEN
-        assertThrows(BookingNotFoundException.class, () -> {
-            paymentController.createPaymentIntent(invalidBookingId);
-        });
-
-        verify(bookingService, times(1)).getBooking(invalidBookingId);
-        verifyNoInteractions(paymentService);
+    private ResponseEntity<PropertyResponseDto> createPropertyInDb() {
+        return testRestTemplate
+                .postForEntity(BASE_PROPERTY_URL, propertyRequestDto, PropertyResponseDto.class);
     }
-
-//    @Test
-//    void createPaymentIntent_ShouldHandlePaymentServiceFailure() {
-//        // GIVEN
-//        UUID bookingId = UUID.randomUUID();
-//        BookingResponseDto bookingResponse = new BookingResponseDto();
-//        bookingResponse.setTotalPrice(BigDecimal.valueOf(150.00));
-//
-//        Map<String, Object> expectedResponse = new HashMap<>();
-//        expectedResponse.put("error", "Payment creation failed.");
-//
-//        when(bookingService.getBooking(eq(bookingId))).thenReturn(bookingResponse);
-//        when(paymentService.createPaymentIntent(eq("150.00"))).thenReturn(expectedResponse);
-//
-//        // WHEN
-//        Map<String, Object> response = paymentController.createPaymentIntent(bookingId);
-//
-//        // THEN
-//        assertEquals("Payment creation failed.", response.get("error"));
-//        verify(bookingService, times(1)).getBooking(bookingId);
-//        verify(paymentService, times(1)).createPaymentIntent("150.00");
-//    }
 }
