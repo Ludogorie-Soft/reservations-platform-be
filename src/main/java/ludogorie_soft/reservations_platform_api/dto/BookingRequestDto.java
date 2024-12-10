ffcb4f7829cf7ddff94cb365c58c9f5b40d40e89
HristoSLV <hristoivanovslavchev@gmail.com>
2024-12-10 12:44:31 +0200

    Change testCreateBookingShouldThrowWhenTheDatesAreInThePast to use Bean Validation instead of service validation

--- src/main/java/ludogorie_soft/reservations_platform_api/dto/BookingRequestDto.java
+++ src/main/java/ludogorie_soft/reservations_platform_api/dto/BookingRequestDto.java
@@ -1,5 +1,6 @@
 package ludogorie_soft.reservations_platform_api.dto;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -15,9 +16,11 @@ public class BookingRequestDto {
     private UUID propertyId;
 
     @NotNull(message = "Start date cannot be null")
+    @Future(message = "Start date cannot be in the past")
     private Date startDate;
 
     @NotNull(message = "End date cannot be null")
+    @Future(message = "End date cannot be in the past")
     private Date endDate;
 
     @NotNull(message = "Description cannot be null")
