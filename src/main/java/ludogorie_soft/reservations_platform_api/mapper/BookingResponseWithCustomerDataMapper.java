14863bb89c90e08fb67cc9839a73677f6699d379
HristoSLV <hristoivanovslavchev@gmail.com>
2025-01-05 18:02:49 +0200

    fix tests

--- src/main/java/ludogorie_soft/reservations_platform_api/mapper/BookingResponseWithCustomerDataMapper.java
+++ src/main/java/ludogorie_soft/reservations_platform_api/mapper/BookingResponseWithCustomerDataMapper.java
@@ -8,42 +8,28 @@ import ludogorie_soft.reservations_platform_api.entity.Booking;
 import ludogorie_soft.reservations_platform_api.entity.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 @NoArgsConstructor
 public class BookingResponseWithCustomerDataMapper {
 
     public static BookingResponseWithCustomerDataDto toBookingWithCustomerDataDto(Booking booking, Customer customer) {
-        BookingResponseWithCustomerDataDto dto = new BookingResponseWithCustomerDataDto();
-        BookingResponseDto bookingResponseDto = new BookingResponseDto();
-        BookingRequestCustomerDataDto bookingRequestCustomerDataDto = new BookingRequestCustomerDataDto();
+        BookingResponseDto bookingResponseDto = createBookingResponseDto(booking);
+        BookingRequestCustomerDataDto bookingRequestCustomerDataDto = createBookingRequestCustomerDataDto(booking.getId(), customer);
 
-        bookingResponseDto.setId(booking.getId());
-        bookingResponseDto.setStartDate(booking.getStartDate().toString());
-        bookingResponseDto.setEndDate(booking.getEndDate().toString());
-        bookingResponseDto.setDescription(booking.getDescription());
-        bookingResponseDto.setAdultCount(booking.getAdultCount());
-        bookingResponseDto.setChildrenCount(booking.getChildrenCount());
-        bookingResponseDto.setBabiesCount(booking.getBabiesCount());
-        bookingResponseDto.setPetContent(booking.isPetContent());
-        bookingResponseDto.setTotalPrice(booking.getTotalPrice());
-
-        bookingRequestCustomerDataDto.setBookingId(booking.getId());
-        bookingRequestCustomerDataDto.setFirstName(customer.getFirstName());
-        bookingRequestCustomerDataDto.setLastName(customer.getLastName());
-        bookingRequestCustomerDataDto.setEmail(customer.getEmail());
-        bookingRequestCustomerDataDto.setPhoneNumber(customer.getPhoneNumber());
-
-        dto.setBookingResponseDto(bookingResponseDto);
-        dto.setBookingRequestCustomerDataDto(bookingRequestCustomerDataDto);
-
-        return dto;
+        return createDto(bookingResponseDto, bookingRequestCustomerDataDto);
     }
 
     public static BookingResponseWithCustomerDataDto toBookingWithCustomerDataDto(Booking booking) {
-        BookingResponseWithCustomerDataDto dto = new BookingResponseWithCustomerDataDto();
-        BookingResponseDto bookingResponseDto = new BookingResponseDto();
-        BookingRequestCustomerDataDto bookingRequestCustomerDataDto = new BookingRequestCustomerDataDto();
+        BookingResponseDto bookingResponseDto = createBookingResponseDto(booking);
+        BookingRequestCustomerDataDto bookingRequestCustomerDataDto = createBookingRequestCustomerDataDto(booking.getId(), booking.getCustomer());
 
+        return createDto(bookingResponseDto, bookingRequestCustomerDataDto);
+    }
+
+    private static BookingResponseDto createBookingResponseDto(Booking booking) {
+        BookingResponseDto bookingResponseDto = new BookingResponseDto();
         bookingResponseDto.setId(booking.getId());
         bookingResponseDto.setStartDate(booking.getStartDate().toString());
         bookingResponseDto.setEndDate(booking.getEndDate().toString());
@@ -53,18 +39,27 @@ public class BookingResponseWithCustomerDataMapper {
         bookingResponseDto.setBabiesCount(booking.getBabiesCount());
         bookingResponseDto.setPetContent(booking.isPetContent());
         bookingResponseDto.setTotalPrice(booking.getTotalPrice());
+        return bookingResponseDto;
+    }
 
-        bookingRequestCustomerDataDto.setBookingId(booking.getId());
-        if (booking.getCustomer() != null) {
-            bookingRequestCustomerDataDto.setFirstName(booking.getCustomer().getFirstName());
-            bookingRequestCustomerDataDto.setLastName(booking.getCustomer().getLastName());
-            bookingRequestCustomerDataDto.setEmail(booking.getCustomer().getEmail());
-            bookingRequestCustomerDataDto.setPhoneNumber(booking.getCustomer().getPhoneNumber());
+    private static BookingRequestCustomerDataDto createBookingRequestCustomerDataDto(UUID bookingId, Customer customer) {
+        BookingRequestCustomerDataDto bookingRequestCustomerDataDto = new BookingRequestCustomerDataDto();
+        bookingRequestCustomerDataDto.setBookingId(bookingId);
+        if (customer != null) {
+            bookingRequestCustomerDataDto.setFirstName(customer.getFirstName());
+            bookingRequestCustomerDataDto.setLastName(customer.getLastName());
+            bookingRequestCustomerDataDto.setEmail(customer.getEmail());
+            bookingRequestCustomerDataDto.setPhoneNumber(customer.getPhoneNumber());
         }
+        return bookingRequestCustomerDataDto;
+    }
 
+    private static BookingResponseWithCustomerDataDto createDto(BookingResponseDto bookingResponseDto,
+                                                                BookingRequestCustomerDataDto bookingRequestCustomerDataDto) {
+
+        BookingResponseWithCustomerDataDto dto = new BookingResponseWithCustomerDataDto();
         dto.setBookingResponseDto(bookingResponseDto);
         dto.setBookingRequestCustomerDataDto(bookingRequestCustomerDataDto);
-
         return dto;
     }
 }
