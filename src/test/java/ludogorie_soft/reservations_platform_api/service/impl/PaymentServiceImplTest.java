b5c4155e1071d6a7b7a2446626dc78ca526d295c
HristoSLV <hristoivanovslavchev@gmail.com>
2025-02-04 11:21:39 +0200

    test fix

--- src/test/java/ludogorie_soft/reservations_platform_api/service/impl/PaymentServiceImplTest.java
+++ src/test/java/ludogorie_soft/reservations_platform_api/service/impl/PaymentServiceImplTest.java
@@ -23,7 +23,6 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
