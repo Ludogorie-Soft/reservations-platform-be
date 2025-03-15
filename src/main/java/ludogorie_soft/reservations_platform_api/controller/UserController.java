08eff5ba73dcad008a22a14a603d83388bb303a7
HristoSLV <99892358+HristoSLV@users.noreply.github.com>
2025-03-15 16:13:18 +0200

    add user list functionality (#26)
    add user list functionality

---
+++ src/main/java/ludogorie_soft/reservations_platform_api/controller/UserController.java
@@ -0,0 +1,26 @@
+package ludogorie_soft.reservations_platform_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import ludogorie_soft.reservations_platform_api.dto.UserResponseDto;
+import ludogorie_soft.reservations_platform_api.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
